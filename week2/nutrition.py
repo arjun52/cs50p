36d0ed1e2ee78780e2192b6b1e874d318141c03f
arjun52 <58993064+arjun52@users.noreply.github.com>
2022-07-16 13:53:26 -0400

    Create nutrition.py

---
+++ week2/nutrition.py
@@ -0,0 +1,4 @@
+dict = {"apple": "130", "Avocado": "50", "banana": "110", "cantaloupe": "50", "grapefruit": "60", "grapes": "90", "honeydew melon": "50", "Kiwifruit": "90", "lemon": "15", "lime": "20", "nectarine": "60", "orange": "80", "peach": "60", "pear": "100", "pineapple": "50", "plums": "70", "strawberries": "50", "Sweet Cherries": "100", "tangerine": "50", "watermon": "80", }
+i = input("item:")
+if i in dict:
+    print(dict[i])
